e315891b7f6075898352086dfdc112bdf7af2b38
JPiedra <javierpiedraiglesias@gmail.com>
2025-03-31 10:23:31 +0200

    update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path';
+
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
-  base: "/quiz/"
+  base: "/quiz/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        home: resolve(__dirname, 'src/pages/home.html'),
+        geography: resolve(__dirname, 'src/pages/geography.html'),
+        history: resolve(__dirname, 'src/pages/history.html'),
+        math: resolve(__dirname, 'src/pages/math.html'),
+        literature: resolve(__dirname, 'src/pages/literature.html'),
+        science: resolve(__dirname, 'src/pages/science.html'),
+      }
+    }
+  }
 })
