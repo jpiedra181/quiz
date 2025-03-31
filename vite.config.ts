378973901161832df8bf150fa9802e7cade4a8c7
JPiedra <javierpiedraiglesias@gmail.com>
2025-03-31 10:29:12 +0200

    run locally

--- vite.config.ts
+++ vite.config.ts
@@ -7,18 +7,18 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
-  base: "/quiz/",
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        home: resolve(__dirname, 'src/pages/home.html'),
-        geography: resolve(__dirname, 'src/pages/geography.html'),
-        history: resolve(__dirname, 'src/pages/history.html'),
-        math: resolve(__dirname, 'src/pages/math.html'),
-        literature: resolve(__dirname, 'src/pages/literature.html'),
-        science: resolve(__dirname, 'src/pages/science.html'),
-      }
-    }
-  }
+  // base: "/quiz/",
+  // build: {
+  //   rollupOptions: {
+  //     input: {
+  //       main: resolve(__dirname, 'index.html'),
+  //       home: resolve(__dirname, 'src/pages/home.html'),
+  //       geography: resolve(__dirname, 'src/pages/geography.html'),
+  //       history: resolve(__dirname, 'src/pages/history.html'),
+  //       math: resolve(__dirname, 'src/pages/math.html'),
+  //       literature: resolve(__dirname, 'src/pages/literature.html'),
+  //       science: resolve(__dirname, 'src/pages/science.html'),
+  //     }
+  //   }
+  // }
 })
