4bdb633f62014be356461147bf1cb58dcc7ef003
JPiedra <javierpiedraiglesias@gmail.com>
2025-04-05 15:15:40 +0200

    remove docs folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,10 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-import { resolve } from 'path';
 
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
-  // base: "/quiz/",
-  // build: {
-  //   rollupOptions: {
-  //     input: {
-  //       main: resolve(__dirname, 'index.html'),
-  //       home: resolve(__dirname, 'src/pages/home.html'),
-  //       geography: resolve(__dirname, 'src/pages/geography.html'),
-  //       history: resolve(__dirname, 'src/pages/history.html'),
-  //       math: resolve(__dirname, 'src/pages/math.html'),
-  //       literature: resolve(__dirname, 'src/pages/literature.html'),
-  //       science: resolve(__dirname, 'src/pages/science.html'),
-  //     }
-  //   }
-  // }
+  base: '/quiz/',
 })
