2d41ff0728cc8546ec8b23ce6004cbbd62db692e
JPiedra <javierpiedraiglesias@gmail.com>
2025-04-05 18:46:11 +0200

    vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -14,12 +14,12 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        geography: resolve(__dirname, 'pages/geography/index.html'),
-        history: resolve(__dirname, 'pages/history/index.html'),
-        science: resolve(__dirname, 'pages/science/index.html'),
-        home: resolve(__dirname, 'pages/home/index.html'),
-        literature: resolve(__dirname, 'pages/literature/index.html'),
-        math: resolve(__dirname, 'pages/math/index.html'),
+        geography: resolve(__dirname, 'geography/index.html'),
+        history: resolve(__dirname, 'history/index.html'),
+        science: resolve(__dirname, 'science/index.html'),
+        home: resolve(__dirname, 'home/index.html'),
+        literature: resolve(__dirname, 'literature/index.html'),
+        math: resolve(__dirname, 'math/index.html'),
       },
     }
   }
