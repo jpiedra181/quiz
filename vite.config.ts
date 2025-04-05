eb36b17131d480f2eedbee1f22b81b7cc95430d8
JPiedra <javierpiedraiglesias@gmail.com>
2025-04-05 15:33:03 +0200

    some changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-
+import path from 'path'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
   base: '/quiz/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        home: path.resolve(__dirname, 'home.html'),
+        geography: path.resolve(__dirname, 'geography.html'),
+        history: path.resolve(__dirname, 'history.html'),
+        science: path.resolve(__dirname, 'science.html'),
+        literature: path.resolve(__dirname, 'literature.html'),
+        math: path.resolve(__dirname, 'math.html')
+      }
+    }
+  }
 })
