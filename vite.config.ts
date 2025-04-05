47738a881ef56264c203a44fe37f94457aed270a
JPiedra <javierpiedraiglesias@gmail.com>
2025-04-05 17:21:02 +0200

    more changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [
@@ -10,14 +13,14 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, 'index.html'),
-        home: path.resolve(__dirname, './src/pages/home.html'),
-        geography: path.resolve(__dirname, './src/pages/geography.html'),
-        history: path.resolve(__dirname, './src/pages/history.html'),
-        science: path.resolve(__dirname, './src/pages/science.html'),
-        literature: path.resolve(__dirname, './src/pages/literature.html'),
-        math: path.resolve(__dirname, './src/pages/math.html')
-      }
+        main: resolve(__dirname, 'index.html'),
+        geography: resolve(__dirname, 'pages/geography/index.html'),
+        history: resolve(__dirname, 'pages/history/index.html'),
+        science: resolve(__dirname, 'pages/science/index.html'),
+        home: resolve(__dirname, 'pages/home/index.html'),
+        literature: resolve(__dirname, 'pages/literature/index.html'),
+        math: resolve(__dirname, 'pages/math/index.html'),
+      },
     }
   }
 })
